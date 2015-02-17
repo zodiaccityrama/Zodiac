c34e71ecb76e665ef7fe43fbf67185f79c9c9941
Ionuț Neagu <neagu.ionut@yahoo.com>
2015-02-17 23:56:19 +0200

    Versiunea 0.0.2 - Test analytics

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,12 +7,12 @@ zodiacC.controller('AppCtrl', function ($scope, $rootScope) {
 
 zodiacC.controller('HoroscopCtrl', function ($scope, $rootScope) {
     $rootScope.zodiiRow = $rootScope.zodiiToRow(3);
-    if (ionic.Platform.isWebView()) {
-        alert(gaPlugin.trackPage(nativePluginResultHandler, nativePluginErrorHandler, "some.url.com"));
-    }
+    //if (ionic.Platform.isWebView()) {
+        //alert(gaPlugin.trackPage(nativePluginResultHandler, nativePluginErrorHandler, "some.url.com"));
+    //}
 });
 
-zodiacC.controller('HoroscopZodiaCtrl', function ($scope, $rootScope, $stateParams) {
+zodiacC.controller('HoroscopZodiaCtrl', function ($scope, $rootScope, $stateParams, $http) {
     $scope.zodia = $stateParams.zodia;
     ionic.Platform.ready(function () {
         if (ionic.Platform.isWebView()) {
@@ -27,6 +27,12 @@ zodiacC.controller('HoroscopZodiaCtrl', function ($scope, $rootScope, $statePara
 
             // gaPlugin.trackPage(function () {}, function () {}, "/horoscop/" + $rootScope[$scope.zodia].iconita);
         }
+    });
+
+     $http.get('http://www.cityrama.ro/zodiac/jsons/get_horoscop.php', {id_zodie: $scope.zodia})
+         .success(function (data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
     });
 
 });
